34c763d4662d426b7ed5bf205e83fe4845bea13e
SaumyaS <saumya.proof@gmail.com>
2014-01-02 13:37:06 -0500

    test

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -132,6 +132,7 @@ $$(".nav_wrapper"){
 			attributes(data-ur-toggler-component: "content", data-ur-id: "submenu")
 			$(".//a"){
 				attribute("class", "_sub2")
+				add_class("test")
 			}
 		}
 	}
