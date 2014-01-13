f25d56d943c1a8650203693ac8b72fdceb2e519d
SaumyaS <saumya.proof@gmail.com>
2014-01-13 13:34:05 -0500

    move scripts to bottom

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -42,14 +42,14 @@ $("./head"){
 	}
 	$("./script[10]"){
 		insert_after("script", type: "text/javascript", "  var _gaq = _gaq || [];
-		  _gaq.push(['_setAccount', 'UA-10289527-4']);
-		  _gaq.push(['_trackPageview']);
+			_gaq.push(['_setAccount', 'UA-10289527-4']);
+			_gaq.push(['_trackPageview']);
 
-		  (function() {
-		    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-		    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-		    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-		  })();
+			(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+			})();
 		")
 	}
 }
@@ -59,23 +59,18 @@ $("/html"){
   remove(".//script[contains(@src,'navigation.js')]")
 
 
-  # remove(".//script[contains(@src,'CommonContextsDeclarations.js')]")
-  # remove(".//script[contains(@src,'CommonControllersDeclaration.js')]")
-  # remove(".//script[contains(@src,'CategoryDisplay.js')]")
-  # remove(".//script[contains(@src,'MessageHelper.js')]")
-  # remove(".//script[contains(@src,'ServicesDeclaration.js')]")
-  # remove(".//script[contains(@src,'dojo.js')]")
-  # remove(".//script[contains(@src,'ServicesEventMapping.js')]")
+	# remove(".//script[contains(@src,'CommonContextsDeclarations.js')]")
+	# remove(".//script[contains(@src,'CommonControllersDeclaration.js')]")
+	# remove(".//script[contains(@src,'CategoryDisplay.js')]")
+	# remove(".//script[contains(@src,'MessageHelper.js')]")
+	# remove(".//script[contains(@src,'ServicesDeclaration.js')]")
+	# remove(".//script[contains(@src,'dojo.js')]")
+	# remove(".//script[contains(@src,'ServicesEventMapping.js')]")
   	$("./body"){
-  # 		move_here("//script[contains(@src,'CommonContextsDeclarations.js')]", "top")
-		# move_here("//script[contains(@src,'CommonControllersDeclaration.js')]", "top")
-		# move_here("//script[contains(@src,'CategoryDisplay.js')]", "top")
-		# move_here("//script[contains(@src,'MessageHelper.js')]", "top")
-		# move_here("//script[contains(@src,'ServicesDeclaration.js')]", "top")
-		# move_here("//script[contains(@src,'dojo.js')]", "top")
-		# move_here("//script[contains(@src,'ServicesEventMapping.js')]", "top")
-		# move_here(".//script[contains(@src,'jcarousel.min.js')]", "top")
-
+  		move_here("//script", "bottom")
+  		# $("./script[contains(@src, 'main.js']"){
+  		# 	move_to("//head")
+  		# }
 
 		# Removes all the breakpoints
 		match($path){
