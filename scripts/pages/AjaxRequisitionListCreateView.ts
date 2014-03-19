40767473a07a08b7e493f401c1eccd359ffc94f0
Saumya Sinha <saumya.proof@gmail.com>
2014-03-19 19:25:26 -0400

    fixed styling of speed build page, line styling

--- scripts/pages/AjaxRequisitionListCreateView.ts
+++ scripts/pages/AjaxRequisitionListCreateView.ts
@@ -60,9 +60,6 @@ $(".//div[@class='main_container']"){
 					$("./div[contains(@class,'mw_was_tr')]"){
 						# attributes(class: "_items")
 						match($host){
-							with(/m.whitecap.com/){
-								
-							}
 							else(){
 								attributes(class: "color-bg disable")
 							}
