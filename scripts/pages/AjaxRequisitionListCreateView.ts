3709723943d630d510ab84cd7b2bf3ba6c6d613e
SaumyaS <saumya.proof@gmail.com>
2014-03-25 15:48:16 -0400

    remove image reduction on speedbuild

--- scripts/pages/AjaxRequisitionListCreateView.ts
+++ scripts/pages/AjaxRequisitionListCreateView.ts
@@ -141,20 +141,20 @@ $(".//div[@class='main_container']"){
 						$("./div[6]"){
 							attributes(class: "_imgContainer")
 							# Reduce image size, optimize
-							$(".//img") {
-								perf.optimize_image("src") {
-									# Change image formats: jpeg, png, and webp!
-									perf.format("jpeg")
+							# $(".//img") {
+							# 	perf.optimize_image("src") {
+							# 		# Change image formats: jpeg, png, and webp!
+							# 		perf.format("jpeg")
 
-									# # Resize the pixels
-									perf.width("52")
-									perf.height("52")
+							# 		# # Resize the pixels
+							# 		perf.width("52")
+							# 		perf.height("52")
 
-									# Change the image quality: 0 - 100
-									# This affects the image resolution
-									perf.quality("100")
-								}
-							}
+							# 		# Change the image quality: 0 - 100
+							# 		# This affects the image resolution
+							# 		perf.quality("100")
+							# 	}
+							# }
 							# insert_top("label", class: "_imgTitle", "Image")
 						}
 						$("./div[7]"){
