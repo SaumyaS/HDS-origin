5549b2de0b1889501687ac67293aa47b5350a300
SaumyaS <saumya.proof@gmail.com>
2014-03-26 10:23:34 -0400

    iframe_history

--- scripts/sections/header.ts
+++ scripts/sections/header.ts
@@ -34,7 +34,7 @@ $("./head"){
 	insert("link", rel: "apple-touch-icon", sizes: "114x114", href: asset("images/WhiteCap.png"))
 
     $("./script[contains(@src, 'dojo.js')]") {
-      insert_after("script", "dojo.config.dojoIframeHistoryUrl = 'http://"+$host+"/wcsstore/dojo15/dojo/resources/iframe_history.html'", type:"text/javascript")
+      insert_after("script", "dojo.config.dojoIframeHistoryUrl = 'https://"+$host+"/wcsstore/dojo15/dojo/resources/iframe_history.html'", type:"text/javascript")
     }
 
 	$("./script"){
