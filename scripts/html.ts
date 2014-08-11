e21449ef56219b63cf6868b6122c7b93d75c0509
SaumyaS <saumya.proof@gmail.com>
2014-08-11 15:05:03 -0400

    Defect 2905
    remove moovweb modification for Branch Landing Page

--- scripts/html.ts
+++ scripts/html.ts
@@ -1,29 +1,37 @@
 # HTML Transformations go here
 
 $("/html") {
-  rewrite_links()
-  absolutize_srcs()
 
-    
-  # Add the mobile meta tags 
-  clean_mobile_meta_tags()
+  match($path){
+    with(/Branch_114/){
+    }
+    else(){
+      rewrite_links()
+      absolutize_srcs()
 
-    
-  # Needed to begin mobilizing
-  remove_all_styles()
-  remove_html_comments()
+        
+      # Add the mobile meta tags 
+      clean_mobile_meta_tags()
+
+        
+      # Needed to begin mobilizing
+      remove_all_styles()
+      remove_html_comments()
+      
+      # Late load all the images on the site
+      #lateload()
+
+      add_assets()
+
+      @import sections/header.ts
+      @import sections/footer.ts
+
+      @import sections/pagination.ts
+
+      @import mappings.ts
+    }
+  }
   
-  # Late load all the images on the site
-  #lateload()
-
-  add_assets()
-
-  @import sections/header.ts
-  @import sections/footer.ts
-
-  @import sections/pagination.ts
-
-  @import mappings.ts
 
 }
 
