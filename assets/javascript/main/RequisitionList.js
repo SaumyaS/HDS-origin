749b3a0421cb059e55aaac1c0e5697ed192eafcc
Saumya Sinha <saumya.proof@gmail.com>
2014-09-24 15:23:31 -0400

    disable quantity field

--- assets/javascript/main/RequisitionList.js
+++ assets/javascript/main/RequisitionList.js
@@ -5,6 +5,7 @@ function deselectAllRowM(){
 	for(var x=1;x<=totalLineItems;x++) {
 	    document.getElementById("chkselected_"+x).value="false";
 	    document.getElementById("quantityHidden_" + x).disabled=true;
+	    document.getElementById("quantity_" + x).disabled=true;
 	}
 	var inputs_in_table = document.getElementById("detailsTable").getElementsByTagName("input");
 	var inputLength = inputs_in_table.length;
@@ -12,14 +13,6 @@ function deselectAllRowM(){
         if(inputs_in_table[i].type == "checkbox") {
         	if (inputs_in_table[i].checked) {
         			inputs_in_table[i].click();
-        	}
-        }
-    }
-    var inputs_out_table = document.getElementById("outsideTable").getElementsByTagName("input");
-    for(var i=0; i<inputs_out_table.length; i++) {
-        if(inputs_out_table[i].type == "checkbox") {
-        	if (inputs_out_table[i].checked) {
-        		inputs_out_table[i].click();
         	}
         }
     }
@@ -38,30 +31,30 @@ function selectAllRowM(){
 			document.getElementById("quantity_" + i).disabled=true;
     	}
     	else{
-    	if(document.getElementById("totalPrice_" + i) != null && document.getElementById("totalPrice_" + i) != undefined){
-    		var numberString = new String(document.getElementById("totalPrice_" + i).innerHTML);
+	    	if(document.getElementById("totalPrice_" + i) != null && document.getElementById("totalPrice_" + i) != undefined){
+	    		var numberString = new String(document.getElementById("totalPrice_" + i).innerHTML);
 
 
-    		if(numberString.indexOf("Price") != -1){
-				document.getElementById("checkbox" + i).click();
-				document.getElementById("checkboxOrg" + i).click();
-				document.getElementById("checkbox" + i).disabled=true;
-				document.getElementById("checkboxOrg" + i).disabled=true;
-				document.getElementById("quantity_" + i).disabled=true;
+	    		if(numberString.indexOf("Price") != -1){
+					document.getElementById("checkbox" + i).click();
+					document.getElementById("checkboxOrg" + i).click();
+					document.getElementById("checkbox" + i).disabled=true;
+					document.getElementById("checkboxOrg" + i).disabled=true;
+					document.getElementById("quantity_" + i).disabled=true;
+				}
+				if(numberString.indexOf("Quote") != -1){
+					document.getElementById("checkbox" + i).click();
+					document.getElementById("checkboxOrg" + i).click();
+					document.getElementById("checkbox" + i).disabled=true;
+					document.getElementById("checkboxOrg" + i).disabled=true;
+					document.getElementById("quantity_" + i).disabled=true;
+				}
+				else{
+					document.getElementById("quantity_" + i).disabled=false;
+				}
 			}
-			if(numberString.indexOf("Quote") != -1){
-				document.getElementById("checkbox" + i).click();
-				document.getElementById("checkboxOrg" + i).click();
-				document.getElementById("checkbox" + i).disabled=true;
-				document.getElementById("checkboxOrg" + i).disabled=true;
-				document.getElementById("quantity_" + i).disabled=true;
-			}
-			else{
-				document.getElementById("quantity_" + i).disabled=false;
-			}
-		}
-      }
-	  }
+      	}
+	}
 	var inputs_in_table = document.getElementById("detailsTable").getElementsByTagName("input");
 	var inputLength = inputs_in_table.length;
     for(var i=0; i<inputs_in_table.length; i++) {
@@ -71,17 +64,6 @@ function selectAllRowM(){
         	}
         }
     }
-    var inputs_out_table = document.getElementById("outsideTable").getElementsByTagName("input");
-    for(var i=0; i<inputs_out_table.length; i++) {
-        if(inputs_out_table[i].type == "checkbox") {
-        	if (!inputs_out_table[i].checked && inputs_out_table[i].disabled != "true") {
-        		inputs_out_table[i].click();
-        	}
-        }
-    }
-
-
-
     updateAllListItemsM("selectItems","Y");
 }
 
