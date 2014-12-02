97e7b153eef05c539327f8a6648af0acdffa7778
SaumyaS <saumya.proof@gmail.com>
2014-12-02 15:25:00 -0500

    Credit Account Page

--- scripts/pages/creditAccount.ts
+++ scripts/pages/creditAccount.ts
@@ -5,4 +5,53 @@ $$("#ad_17601"){
 
 $(".//div[@class='main_container']"){
 	add_class("_creditAccount")
+
+	$(".//div[@id='WC_ContentAreaESpot_div_1_148001']"){
+		$("./h1"){
+			remove()
+		}
+		$("./hr"){
+			remove() 
+		}
+
+		$(".//div[@id='WC_ContentAreaESpot_div_22_1']"){
+			$("./img[@name='Finance']"){
+				attributes(class: "_heroBanner")
+			}
+			$("p[1]"){
+				attributes(class: "_backButton", align: "")
+				$("./a"){
+					add_class("expand")
+				}
+			}
+			$("./p[2]"){
+				attributes(class: "_firstPara")
+			}
+			$("./div[@class='mw_was_table']"){
+				attributes(class: "creditTable")
+				$("./div"){
+					attributes(class: "_creditRows", id: "creditRows" + index())
+					$("./div"){
+						attributes(class: "_creditContents", id: "creditContents" + index())
+						$("./div"){
+							$("./div"){
+								attributes(class: "_creditItem")
+							}
+							$("./div[1]"){
+								attributes(class: "_creditHeader", style: "")
+							}
+							$("./div[@class='_creditItem']"){
+								$("./div[1]"){
+									attributes(class: "_plus")
+								}
+								$("./div[2]"){
+									attributes(class: "_content")
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+	}
 }
