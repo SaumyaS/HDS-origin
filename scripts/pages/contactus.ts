1a5d8b0230f0398f1684b5c97f604e4a0fc8a86d
Saumya Sinha <saumya.proof@gmail.com>
2015-02-13 10:24:26 -0500

    change  number to link

--- scripts/pages/contactus.ts
+++ scripts/pages/contactus.ts
@@ -9,6 +9,15 @@ $("/html"){
 	$("//*[@id='main_content_wrapper']/div/div"){
 		$("./div"){
 			attribute("class", "_contact")
+			$(".//div[@data-mw-id='mw_dump_td3']"){
+				$("./p[1]"){
+					attributes(class: "_phone")
+					text(){
+						replace(/ 800-944-8322./, ":")
+					}
+					insert("a", href: "tel:800-944-8322", "800-944-8322")
+				}
+			}
 		}
 	}
 }
