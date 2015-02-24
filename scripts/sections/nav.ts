f53a0d0be9490d5a74fe05a2d306cec3168fdb0c
SaumyaS <saumya.sinha@hdsupply.com>
2015-02-24 09:43:04 -0500

    nav bar, changes

--- scripts/sections/nav.ts
+++ scripts/sections/nav.ts
@@ -47,6 +47,64 @@ $$("body"){
 				}
 				$("./li[6]"){
 					attributes(class: "_navToggler", id: "quickOrderToggler")
+					$("./button"){
+						attributes(style: "")
+						remove()
+					}
+					insert_top("div", class: "_QObutton", "Quick Order")
+
+					move_here("../../div[@id='sections_panel']")
+
+					# Create Toggler
+					attributes(data-ur-set: "toggler")
+					$("./div[@class='_QObutton']"){
+						attributes(data-ur-toggler-component: "button")
+					}
+					$("./div[@id='sections_panel']"){
+						attributes(data-ur-toggler-component: "content")
+					}
+
+					$("./div[@id='sections_panel']"){
+						$(".//div[@class='quick_order_wrapper']"){
+							insert_top("div", class: "_qoptoolTipContainer"){
+								move_here("../div[@class='qo_title']")
+								move_here("../a")
+								insert("div", class: "_qoptoolTipInst", "The Quick Order Pad is an easy way to fill your cart with online items. Simply enter your Item, SKU, or Part Number, enter a Quantity, then Add to Cart.")
+							}
+							$("./div[@class='_qoptoolTipContainer']"){
+								attributes(data-ur-set: "tabs", data-ur-closeable: "true")
+								$("./a"){
+									attributes(class: "_qoptoolTip", data-ur-tabs-component: "button", data-ur-tab-id: "test")
+								}
+								$("./div[@class='_qoptoolTipInst']"){
+									attributes(data-ur-tabs-component: "content", data-ur-tab-id: "test")
+								}
+								
+
+								
+							}
+						}
+						$(".//div[contains(@class,'qo-table')]"){
+							$("./div[1]"){
+								attributes(class: "_qopHeader")
+								$("./div"){
+									attributes(class: "_qopTitleContainer")
+									$("./div[1]"){
+										attributes(class: "_qopItem")
+									}
+									$("./div[2]"){
+										attributes(class: "_qopQTY")
+									}
+								}
+							}
+						}
+						$(".//div[@class='btn_addtocart']"){
+							$("./a"){
+								attributes(style: "")
+							}
+						}
+					}
+
 				}
 
 			}
